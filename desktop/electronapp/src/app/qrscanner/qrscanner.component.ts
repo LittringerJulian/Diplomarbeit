e73cdd334740532b83325a40ea122c3515cc1d66
Robin Wiesinger <robin.wiesinger@gmail.com>
2020-09-20 13:58:22 +0200

    Update qrscanner.component.ts

--- desktop/electronapp/src/app/qrscanner/qrscanner.component.ts
+++ desktop/electronapp/src/app/qrscanner/qrscanner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { QRCodeModule } from 'angularx-qrcode';
 
 declare var electron: any;
