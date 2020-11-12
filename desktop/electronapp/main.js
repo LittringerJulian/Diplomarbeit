6bd29b0457fdb10d3f4606c9a3eb19aaaa7f7e6c
Robin Wiesinger <53317215+RobinWiesinger@users.noreply.github.com>
2020-11-12 16:17:06 +0100

    commit

--- desktop/electronapp/main.js
+++ desktop/electronapp/main.js
@@ -39,11 +39,14 @@ const WebSocket = require("ws");
 const wsport = process.env.PORT || 80;
 const wss = new WebSocket.Server({ port: wsport, host: "0.0.0.0" });
 
+
 wss.on("connection", function connection(ws, req) {
   console.log("new connection: " + req.socket.remoteAddress);
   ws.on("message", function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
+        client.newvariable=true;
+        console.log("variable="+client.newvariable);
         client.send(data);
       }
     });
